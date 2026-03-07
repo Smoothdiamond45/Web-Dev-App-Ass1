e0438e0c85150f770866a4ac033bbf0132afa30f
Matthew <smoothdiamond45@gmail.com>
2026-03-07 16:02:31 +0000

    troubleshooting

--- routes.js
+++ routes.js
@@ -18,5 +18,20 @@ router.get('/about', about.createView);
 
 import detailsL from './controllers/detailsL.js';
 router.get('/detailsL', detailsL.createView);
+
+import detailsA from './controllers/detailsA.js';
+router.get('/detailsA', detailsA.createView);
+
+import detailsM from './controllers/detailsM.js';
+router.get('/detailsM', detailsM.createView);
+
+import detailsN from './controllers/detailsN.js';
+router.get('/detailsN', detailsN.createView);
+
+import detailsP from './controllers/detailsP.js';
+router.get('/detailsP', detailsP.createView);
+
+import detailsS from './controllers/detailsS.js';
+router.get('/detailsS', detailsS.createView);
 export default router;
 
