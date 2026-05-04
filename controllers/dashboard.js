d2340d3235ed89685c7651d787a718626b767351
Matthew <smoothdiamond45@gmail.com>
2026-05-04 18:07:34 +0100

    undid many things

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,11 +1,14 @@
 // dashboard.js
-import photoStore from '../models/photo-store.json';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const photoStore = require('../models/photo-store.json');
 
 const dashboard = {
   createView(request, response) {
     const viewData = {
       title: 'Dashboard',
-      categories: photoStore.findAll('categories')
+      categories: photoStore.categories
     };
     response.render('dashboard', viewData);
   }
